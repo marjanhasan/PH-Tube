3588aa40a6efa52c37f8272aab8ec7f7e37bca0b
marjanhasan <moonmarjanhasan@gmail.com>
2023-11-24 03:06:57 +0600

    cards container done

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const buttonsContainer = document.getElementById("buttons-container");
+const cardsContainer = document.getElementById("cards-container");
+const oopsContainer = document.getElementById("opps-container");
 
 const displayCategories = () => {
   fetch("https://openapi.programming-hero.com/api/videos/categories")
@@ -33,7 +35,53 @@ const handleButtonClicked = (button, id) => {
 
   fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     .then((res) => res.json())
-    .then((data) => console.log(data.data));
+    .then((data) => handleCards(data.data));
+};
+
+const handleCards = (datas) => {
+  if (datas.length == 0) {
+    cardsContainer.classList.add("hidden");
+    oopsContainer.classList.remove("hidden");
+  } else {
+    oopsContainer.classList.add("hidden");
+    cardsContainer.classList.remove("hidden");
+    cardsContainer.innerHTML = "";
+
+    datas.forEach((data) => {
+      const card = document.createElement("div");
+      card.innerHTML = `
+    <div class="relative max-w-fit">
+          <img
+            class="w-72 h-48 bg-cover bg-center rounded-lg"
+            src="${data.thumbnail}"
+            alt=""
+          />
+          <span
+            class="absolute bottom-3 right-3 text-white p-1 bg-[#171717] rounded"
+            >3hrs 56 min ago</span
+          >
+        </div>
+        <div class="flex py-5">
+          <div class="w-10 h-10">
+            <img class="w-10 h-10 bg-cover bg-center pr-3 rounded-full" src="${data.authors[0].profile_picture}" alt="" />
+          </div>
+          <div>
+            <h2 class="text-base font-bold text-[#171717]">
+              ${data.title}
+            </h2>
+            <p
+              class="text-sm font-normal text-[#171717] pt-2.5 flex items-center gap-1"
+            >
+              ${data.authors[0].profile_name}
+              <img class="h-4 w-4" src="./images/verified.svg" alt="" />
+            </p>
+            <p class="text-sm font-normal text-[#171717] pt-2.5">${data.others.views} views</p>
+          </div>
+        </div>
+    `;
+      cardsContainer.appendChild(card);
+    });
+  }
 };
 
 displayCategories();
